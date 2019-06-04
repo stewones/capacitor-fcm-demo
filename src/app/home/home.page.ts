0b307df9b0edab3bf8bc8fcda23a124291277b0e
Stewan <oi@stewan.io>
2019-06-04 14:30:18 -0300

    chore(): add fcm sample

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Plugins, PushNotification } from '@capacitor/core';
+
+const { PushNotifications } = Plugins;
+import { Platform } from '@ionic/angular';
+// import { FCM } from 'capacitor-fcm';
+// const fcm = new FCM();
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+export class HomePage implements OnInit {
+  session: any;
+
+  notifications: PushNotification[] = [];
+  //
+  // move to fcm demo
+  topicName = 'super-awesome-topic';
+  remoteToken: string;
+
+  constructor(private platform: Platform) {}
+
+  ngOnInit() {
+    PushNotifications.addListener('registration', data => {
+      // alert(JSON.stringify(data));
+      console.log(data);
+    });
+    PushNotifications.register().then(() => alert(`registered for push`));
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotification) => {
+        console.log('notification ' + JSON.stringify(notification));
+        this.notifications.push(notification);
+      }
+    );
+  }
+
+  //
+  // move to fcm demo
+  subscribeTo() {
+    // PushNotifications.register()
+    //   .then(_ => {
+    // fcm
+    //   .subscribeTo({ topic: this.topicName })
+    //   .then(r => alert(`subscribed to topic ${this.topicName}`))
+    //   .catch(err => console.log(err));
+    // })
+    // .catch(err => alert(JSON.stringify(err)));
+  }
+
+  unsubscribeFrom() {
+    // fcm
+    //   .unsubscribeFrom({ topic: 'test' })
+    //   .then(r => alert(`unsubscribed from topic ${this.topicName}`))
+    //   .catch(err => console.log(err));
+    // if (this.platform.is('android')) fcm.deleteInstance();
+  }
+
+  getToken() {
+    // fcm
+    //   .getToken()
+    //   .then(result => {
+    //     this.remoteToken = result.token;
+    //   })
+    //   .catch(err => console.log(err));
+  }
+}
